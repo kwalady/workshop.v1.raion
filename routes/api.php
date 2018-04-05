fa6b3b749e49a5be1f66087386b06b772899074d
kwalady <k.sleepycat@gmail.com>
2018-04-05 12:48:55 +0700

    CRUD routes for Post model added

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,45 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([
+  'prefix'    => 'v1',
+  'namespace' => 'API'
+], function() {
+
+  Route::post('/posts/create', [
+    'as'    => 'posts.create',
+    'uses'  => 'PostController@create'
+  ]);
+
+  Route::get('/post/{id}', [
+    'as'    => 'posts.fetch',
+    'uses'  => 'PostController@fetch'
+  ]);
+
+  Route::get('/posts', [
+    'as'    => 'posts.fetchAll',
+    'uses'  => 'PostController@fetchAll'
+  ]);
+
+  Route::put('/posts/{id}', [
+    'as'    => 'posts.update',
+    'uses'  => 'PostController@update'
+  ]);
+
+  Route::delete('/posts/{id}', [
+    'as'    => 'posts.delete',
+    'uses'  => 'PostController@delete'
+  ]);
+
+  Route::post('/comments/create', [
+    'as'    => 'comments.create',
+    'uses'  => 'CommentController@create'
+  ]);
+
+  Route::delete('/comments/{id}', [
+    'as'    => 'comments.delete',
+    'uses'  => 'CommentController@delete'
+  ]);
+
+});
