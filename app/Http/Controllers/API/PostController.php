f45e28c7bf7511c5b707d1f2085ef0f35c34059d
kwalady <k.sleepycat@gmail.com>
2018-04-05 12:46:55 +0700

    CRUD operations for Post model added

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -2,10 +2,147 @@
 
 namespace App\Http\Controllers\API;
 
-use Illuminate\Http\Request;
+use App\Comment;
+use App\Post;
+use App\Http\Requests\PostCreateRequest;
+use App\Http\Requests\PostFetchRequest;
+use App\Http\Requests\PostFetchAllRequest;
+use App\Http\Requests\PostUpdateRequest;
+use App\Http\Requests\PostDeleteRequest;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    /**
+     * Create a new post
+     *
+     * @param App\Http\Requests\PostCreateRequest $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     **/
+     public function create(PostCreateRequest $request) {
+       try {
+         Post::create([
+           'image'        => $request->file('image')->store('public/images'),
+           'author'       => $request->input('author'),
+           'description'  => $request->input('description'),
+           'likes'        => ''
+         ]);
+       }
+       catch (\Exception $e) {
+         return response()->json([
+           'code'   => 500,
+           'message'=> $e->getMessage()
+         ], 500);
+       }
+
+       return response()->json([
+         'code'   => 201,
+         'message'=> 'Post created'
+       ], 201);
+     }
+
+    /**
+     * Fetch a post based on given id
+     *
+     * @param App\Http\Requests\PostFetchRequest $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     **/
+     public function fetch(PostFetchRequest $request) {
+       try {
+         $post = Post::find($request->input('id'));
+
+         // TODO etch comments associated with this post
+       }
+       catch (\Exception $e) {
+         return response()->json([
+           'code'   => 500,
+           'message'=> $e->getMessage()
+         ], 500);
+       }
+
+       return response()->json([
+         'code' => 200,
+         'data' => $post
+       ], 200);
+     }
+
+    /**
+     * Fetch all posts
+     *
+     * @param App\Http\Requests\PostFetchAllRequest $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     **/
+     public function fetchAll(PostFetchAllRequest $request) {
+       try {
+         $posts = Post::all();
+       }
+       catch (\Exception $e) {
+         return response()->json([
+           'code'   => 500,
+           'message'=> $e->getMessage()
+         ], 500);
+       }
+
+       return response()->json([
+         'code'   => 200,
+         'data'   => $posts
+       ], 200);
+     }
+
+    /**
+     * Update a post
+     *
+     * @param App\Http\Requests\PostUpdateRequest $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     **/
+     public function update(PostUpdateRequest $request) {
+       try {
+         $post = Post::find($request->input('id'))
+          ->fill(
+            ['description' => $request->input('description')]
+          );
+
+         $post->save();
+       }
+       catch (\Exception $e) {
+         return response()->json([
+           'code'   => 500,
+           'message'=> $e->getMessage()
+         ], 500);
+       }
+
+       return response()->json([
+         'code'     => 200,
+         'message'  => 'Post updated'
+       ], 200);
+     }
+
+    /**
+     * Delete a post
+     *
+     * @param App\Http\Requests\PostDeleteRequest $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     **/
+     public function delete(PostDeleteRequest $request) {
+       try {
+         Post::destroy($request->input('id'));
+       }
+       catch (\Exception $e) {
+         return response()->json([
+           'code'   => 500,
+           'message'=> $e->getMessage()
+         ], 500);
+       }
+
+       return response()->json([
+         'code'     => 200,
+         'message'  => 'Post deleted'
+       ], 200);
+     }
 }
